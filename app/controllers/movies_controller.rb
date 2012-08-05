0fdf74ca6608743a2ca5f2b16502034352b37f33
Bruce A. Bergman <bruceb@fatcity.com>
2012-08-05 20:18:24 +0000

    H2-P1 first try

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    # ideally will protect against xss attacks in parameter
+    @movies = Movie.find(:all, :order => params[:sort])
   end
 
   def new
